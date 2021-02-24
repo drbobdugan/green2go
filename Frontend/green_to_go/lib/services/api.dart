65ae0f1600006b44217d1d25a0114d40992c68b1
marianne-kenney <marianne@tclmc.com>
2021-02-23 22:57:39 -0500

    marianne: frontend local url change

--- Frontend/green_to_go/lib/services/api.dart
+++ Frontend/green_to_go/lib/services/api.dart
@@ -1,9 +1,12 @@
 import 'package:http/http.dart';
 
 class API {
+  final String baseURL = '198.199.77.174:5000';
+  final String localURL = '127.0.0.1:5000';
+
   Future postResponse(String path, dynamic params) async {
     Response response = await post(
-      "http://198.199.77.174:5000/$path",
+      "http://$localURL/$path",
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -14,7 +17,7 @@ class API {
   }
 
   Future getResponse(String path, String params) async {
-    Response response = await get("http://198.199.77.174:5000/$path$params");
+    Response response = await get("http://$localURL/$path$params");
     return response.body;
   }
 }
