b1fa4da19843e0874904fc2bb8c102d903214569
Bob Dugan <>
2021-02-24 19:41:48 -0500

    improved the unit test for add user

--- Backend/unitTestUserDao.py
+++ Backend/unitTestUserDao.py
@@ -20,55 +20,52 @@ class unitTestUserDao(unittest.TestCase):
         emailDict={"email": "test42@students.stonehill.edu"}
         dao.deleteUser(emailDict)
     
+    def addTest42User(self):
+        user={
+                "email": "test42@students.stonehill.edu",
+                "password": "password",
+                "firstName": "Test",
+                "lastName": "User",
+                "middleName":"Example",
+                "phoneNum": "7817817811",
+                "role": "RegularUser",
+                "classYear": "2021",
+                "authCode": "1111111",
+                "authTime": "2021-01-01 01:01:01",
+                "lastLogIn": "2021-01-01 01:01:01"}
+        dao = UserDao()
+        return dao.addUser(user)
+    
     def testRegularAddUser(self):
         """
         Test that we can add a user that doesn't exist in the database
         """
-        user={
-                "email": "test42@students.stonehill.edu",
-                "password": "password",
-                "firstName": "Test",
-                "lastName": "User",
-                "middleName":"Example",
-                "phoneNum": "7817817811",
-                "role": "RegularUser",
-                "classYear": "2021",
-                "authCode": "1111111",
-                "authTime": "2021-01-01 01:01:01",
-                "lastLogIn": "2021-01-01 01:01:01"}
-        dao = UserDao()
-        dao.addUser(user)
+        rc, msg = self.addTest42User()
+        self.assertTrue(rc)
 
         email={"email": "test42@students.stonehill.edu"}
+        dao = UserDao()
         rc, getUser = dao.getUser(email)
-        print("user:", user)
-        print("getuser:", getUser)
-        ignore_keys = ["authTime","lastLogIn"]
-        filteredUser  = {k: v for k,v in user.items() if k not in ignore_keys} 
-        filteredGetUser = {k: v for k,v in getUser.items() if k not in ignore_keys} 
-        self.assertEqual(filteredUser,filteredGetUser)
+
+        self.assertTrue(rc)
+        self.assertEqual(email["email"],getUser["email"])
 
     def testAddUserTwice(self):
         """
-        Test that we can add a user that doesn't exist in the database
+        Test that we can't add a user twice
         """
-        user={
-                "email": "test42@students.stonehill.edu",
-                "password": "password",
-                "firstName": "Test",
-                "lastName": "User",
-                "middleName":"Example",
-                "phoneNum": "7817817811",
-                "role": "RegularUser",
-                "classYear": "2021",
-                "authCode": "1111111",
-                "authTime": "2021-01-01 01:01:01",
-                "lastLogIn": "2021-01-01 01:01:01"}
-        dao = UserDao()
-        dao.addUser(user)
-        rc, msg = dao.addUser(user)
+        
+        """
+        First add should work correctly
+        """
+        rc, msg = self.addTest42User()
+        self.assertTrue(rc)
 
-        self.assertEqual(rc,False)
+        """
+        Second add should fail
+        """
+        rc, msg = self.addTest42User()
+        self.assertFalse(rc)
         self.assertEqual(msg,"Duplicate Entry")
 
 if __name__ == '__main__':
