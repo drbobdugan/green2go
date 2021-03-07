908771d9c9a74790ee907d2d2f468d10e4171130
Andrew Petti <apetti1@students.stonehill.edu>
2021-03-06 21:57:35 -0500

    Fixed getContainersForUser

--- Backend/runBackend.py
+++ Backend/runBackend.py
@@ -388,9 +388,9 @@ def getContainersForUser():
         return json.dumps({"success" : False, "message" : authCheck[1]})
     relationship.pop('auth_token', None)
     global containerDao
-    res = containerDao.getRelationship(relationship)
+    res = containerDao.selectAllByEmail(relationship)
     if res[0] is True:
-        res = json.dumps({"success" : res[0], "message" : ""})
+        res = json.dumps({"success" : res[0], "data" : res[1]})
     else:
         res = json.dumps({"success" : res[0], "message" : res[1]})
     return res
