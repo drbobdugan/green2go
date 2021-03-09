aab0bd2a8625fa616ba9b0a975a284b8843d2e84
Ruthie Missett <78761478+rmissett@users.noreply.github.com>
2021-03-08 21:45:21 -0500

    updated locationDao and added unitTestLocationDao

--- Backend/locationDao.py
+++ Backend/locationDao.py
@@ -2,78 +2,67 @@ import mysql.connector
 import json
 from datetime import datetime
 import logging
-class LocationDao:
+from dao import dao
+class LocationDao(dao):
  
-    def __init__(self):
-        logging.basicConfig(filename='locationDao.log', level=logging.DEBUG)
-        self.mydb = mysql.connector.connect(
-            host="198.199.77.174",
-            user="root",
-            password="Capstone2021!",
-            database="sys",
-            buffered=True) 
-
-    def reconnectSql(self):
-        try:
-            self.mydb.shutdown()
-        except:
-            logging.error("Error closing connection: Already disconnected")
-            
-        self.mydb = mysql.connector.connect(
-            host="198.199.77.174",
-            user="root",
-            password="Capstone2021!",
-            database="sys",
-            buffered=True)
-
     #Adds Location with attributes: qrcode of location, description of location, last pickup date at location
-    def insertLocation(self, locDict):  
+    def insertLocation(self, locDict): 
         try:
             logging.info("Entering insertLocation")
             val = []
             val.append(locDict['qrcode'])
             val.append(locDict['description'])
             val.append(locDict['lastPickup'])
-            mycursor = self.mydb.cursor()
             sql = "INSERT INTO location (qrcode, description, lastPickup) VALUES (%s,%s,%s)"
-            mycursor.execute(sql,val)
-            temp = mycursor.rowcount
-            logging.info("Location inserted.")
-            self.mydb.commit()
-            # close cursor 
-            mycursor.close()
+            myresult = self.handleSQL(sql,False,val)
+            if(myresult[0] == False):
+                return myresult
             logging.info("insertLocation successful")
             return True, ""
         except Exception as e:
             logging.error("Error in insertLocation")
             logging.error(str(e))
-            return self.handleError(e, mycursor)
+            self.handleError(e)
     
     #Select Location based on qrcode
     def selectLocation(self,locDict):
         try: 
             logging.info("Entering selectLocation")
             qrcode=locDict['qrcode']
-            mycursor = self.mydb.cursor()
-            mycursor.execute("SELECT * FROM location WHERE qrcode = '" + qrcode + "'")
-            myresult = mycursor.fetchall()
-            # close cursor 
-            mycursor.close()
+            sql = "SELECT * FROM location WHERE qrcode = '" + qrcode + "'"
+            myresult = self.handleSQL(sql,True,None)
+            if(myresult[0] == False):
+                return myresult
+            print("I made it here")
+            myresult = myresult[1][0]
+            print(myresult)
+            locDict={
+                "qrcode": myresult[0],
+                "description": myresult[1],
+                "lastPickup": myresult[2]}
+            print(locDict)
             logging.info("selectLocation successful")
-            return True, {"qrcode" : myresult[0][0]}
+            print("I made it here 2")
+            return True, locDict
         except Exception as e:
             logging.error("Error in selectLocation")
             logging.error(str(e))
-            return self.handleError(e, mycursor)
-            
-    def handleError(self,error, cursor=None):
-        if cursor is not None:
-            cursor.close()
-        error = str(error)
-        if "Duplicate entry" in error:
-            return False,"Duplicate Entry"
-        if "Can't connect to MySQL server" in error:
-            self.reconnectSql()
-            return False, "Could not connect to database please try again"
-        if "list index out of range" in error:
-            return False, "Entry could not be found"
+            self.handleError(e)
+
+    #Delete Location based on qrcode
+    def deleteLocation(self,locDict):
+        qrcode=locDict['qrcode']
+        if(self.selectLocation(locDict)[0] == False):
+            return False, "Location does not exist"
+        try:
+            logging.info("Entering deleteLocation")
+            sql = "DELETE FROM location WHERE qrcode like '" + qrcode + "'"
+            myresult = self.handleSQL(sql,False,None)
+            if(myresult[0] == False):
+                return myresult
+            logging.info("deleteLocation successful")
+            return True, ""
+        except Exception as e:
+            logging.error("Error in deleteLocation")
+            logging.error(str(e))
+            self.handleError(e)
