d38d1dee43f8343dc37c94a658aea10ec3b543b2
Ruthie Missett <78761478+rmissett@users.noreply.github.com>
2021-03-08 22:04:06 -0500

    fixed error in locationDao (passes unit test now)

--- Backend/locationDao.py
+++ Backend/locationDao.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import logging
 from dao import dao
 class LocationDao(dao):
- 
     #Adds Location with attributes: qrcode of location, description of location, last pickup date at location
     def insertLocation(self, locDict): 
         try:
@@ -22,7 +21,7 @@ class LocationDao(dao):
         except Exception as e:
             logging.error("Error in insertLocation")
             logging.error(str(e))
-            self.handleError(e)
+            return self.handleError(e)
     
     #Select Location based on qrcode
     def selectLocation(self,locDict):
@@ -33,21 +32,17 @@ class LocationDao(dao):
             myresult = self.handleSQL(sql,True,None)
             if(myresult[0] == False):
                 return myresult
-            print("I made it here")
             myresult = myresult[1][0]
-            print(myresult)
             locDict={
                 "qrcode": myresult[0],
                 "description": myresult[1],
                 "lastPickup": myresult[2]}
-            print(locDict)
             logging.info("selectLocation successful")
-            print("I made it here 2")
             return True, locDict
         except Exception as e:
             logging.error("Error in selectLocation")
             logging.error(str(e))
-            self.handleError(e)
+            return self.handleError(e)
 
     #Delete Location based on qrcode
     def deleteLocation(self,locDict):
@@ -65,4 +60,4 @@ class LocationDao(dao):
         except Exception as e:
             logging.error("Error in deleteLocation")
             logging.error(str(e))
-            self.handleError(e)
+            return self.handleError(e)
