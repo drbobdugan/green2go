944d8f3973c10b53ce2f714bd4be6e6a69ad70b7
Ruthie Missett <78761478+rmissett@users.noreply.github.com>
2021-03-21 23:17:04 -0400

    updated authDAO and tests

--- Backend/databaseDAOs/authDAO.py
+++ Backend/databaseDAOs/authDAO.py
@@ -11,6 +11,7 @@ class AuthDao(dao):
     def insertAuth(self,auth):
         try:
             logging.info("Entering insertAuth")
+            auth.expires_at = (datetime.now() + timedelta(hours=1)).strftime('%Y-%m-%d %H:%M:%S')
             result = auth.toAuthList()
             sql = "INSERT INTO auth (user,auth_token,refresh_token,expires_at) VALUES (%s,%s,%s,%s)"
             myresult = self.handleSQL(sql,False,result)
@@ -28,33 +29,28 @@ class AuthDao(dao):
             logging.info("Entering selectByEmail")
             sql = "SELECT * FROM auth WHERE user = '" + email + "'"
             myresult = self.handleSQL(sql,True,None)
-            print("myresult: ",myresult)
+            #print("myresult: ",myresult)
             if(myresult[0]==False):
                 return myresult 
             myresult2 = myresult[1][0] #myresult looks like (true,[(user,auth_token,refresh_token,expires_at)])
-            print("myresult2: ",myresult2)
-            auth = Auth(myresult2[0],myresult2[1],myresult2[2],myresult[3])
-            print(auth.getUser)
-            print(auth.auth_token)
-            print(auth.refresh_token)
-            print(auth.expires_at)
+            auth = Auth(myresult2[0],myresult2[1],myresult2[2],myresult2[3])
             return True, auth
         except Exception as e:
             logging.error("Error in selectByEmail")
             logging.error(str(e))
             return self.handleError(e)
 
-    def updateAuth(self,auth):
+    def updateAuth(self,auth): #returns "True" if the auth_token was successfully updated or not
         try:
-            email = auth.user
-            token = auth.auth_token
+            user = auth.user
+            auth_token = auth.auth_token
+            refresh_token = auth.refresh_token
             timeV = (datetime.now() + timedelta(hours=1)).strftime('%Y-%m-%d %H:%M:%S')
-            sql = "UPDATE auth set auth_token='" + str(token) +"', expires_at='" + str(timeV) + "' where user='" +str(email)+ "'"
+            sql = "UPDATE auth set auth_token='" + str(auth_token) +"', expires_at='" + str(timeV) + "' where user='" +str(user)+ "'"
             myresult = self.handleSQL(sql,False,None)
             if(myresult[0]==False):
                 return myresult
-            auth = Auth(myresult[0],myresult[1],myresult[2],myresult[3])
-            return True, auth
+            return True
         except Exception as e:
             logging.error("Error in updateAuth")
             logging.error(str(e))
--- Backend/databaseDAOs/testAuthDAO.py
+++ Backend/databaseDAOs/testAuthDAO.py
@@ -7,16 +7,19 @@ def main():
     Uncomment the desired method to test it
     """
     #testinsertAuth()
-    testSelectByEmail()    #select and update do not work yet
-    testUpdateAuth()
+    testSelectByEmail()  
+    #testUpdateAuth()
     #testDeleteAuth()
 
 def testinsertAuth():
     #auth 1
     auth = Auth("auth1TestUser@students.stonehill.edu","Ggpmy1lEbwaNoIqZmkjBkkzOtskzYquyL11ISH5ij9iRL","l9R51hFTGUgV0LeyJJAkwbSiZL1dfennuGDlPcUJnnNm9","2021-01-01 01:01:01")
-    dao.insertAuth(auth)
+    #dao.insertAuth(auth)
     #auth 2
     auth = Auth("auth2TestUser@students.stonehill.edu","Zgpmy1lEbwaNoIqZmkjBkkzOtskzYquyL11ISH5ij9iRL","Z9R51hFTGUgV0LeyJJAkwbSiZL1dfennuGDlPcUJnnNm9","2021-01-01 01:01:01")
+    #dao.insertAuth(auth)
+    #auth 3
+    auth = Auth("auth3estUser@students.stonehill.edu","Tgpmy1lEbwaNoIqZmkjBkkzOtskzYquyL11ISH5ij9iRL","T9R51hFTGUgV0LeyJJAkwbSiZL1dfennuGDlPcUJnnNm9","2021-01-01 01:01:01")
     dao.insertAuth(auth)
 
 def testSelectByEmail():
@@ -28,8 +31,8 @@ def testSelectByEmail():
     print(dao.selectByEmail(email))
 
 def testUpdateAuth():
-    auth = Auth("auth1TestUser@students.stonehill.edu","Ugpmy1lEbwaNoIqZmkjBkkzOtskzYquyL11ISH5ij9iRL","l9R51hFTGUgV0LeyJJAkwbSiZL1dfennuGDlPcUJnnNm9","2021-01-01 01:01:01")
-    dao.updateAuth(auth)
+    auth = Auth("auth1TestUser@students.stonehill.edu","Rgpmy1lEbwaNoIqZmkjBkkzOtskzYquyL11ISH5ij9iRL","l9R51hFTGUgV0LeyJJAkwbSiZL1dfennuGDlPcUJnnNm9","2021-01-01 01:01:01")
+    print(dao.updateAuth(auth))
 
 def testDeleteAuth():
     auth = Auth("auth1TestUser@students.stonehill.edu","Ggpmy1lEbwaNoIqZmkjBkkzOtskzYquyL11ISH5ij9iRL","l9R51hFTGUgV0LeyJJAkwbSiZL1dfennuGDlPcUJnnNm9","2021-01-01 01:01:01")
