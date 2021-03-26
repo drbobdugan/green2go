25c0d8bbb62248a841283d38acab227fb3d620d5
Patrick OShea <Patrick O'Shea>
2021-03-26 12:28:36 -0400

    Notifications progress

---
+++ Frontend/choose2reuse/android/app/src/main/java/NotificationsMessagingService.java
@@ -0,0 +1,17 @@
+import android.util.Log;
+
+import com.google.firebase.messaging.RemoteMessage;
+import com.pusher.pushnotifications.fcm.MessagingService;
+
+public class NotificationsMessagingService extends MessagingService {
+
+    public NotificationsMessagingService() {
+        super();
+        Log.i("NotificationsSevice", "starting....");
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.i("NotificationsService", "Got a remote message 🎉");
+    }
+}
