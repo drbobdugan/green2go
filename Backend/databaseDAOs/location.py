217ec41f50a333fbc21cea1ab71ad584e9cd5c60
Thomas Hines <tommyhines44@yahoo.com>
2021-04-14 12:49:27 -0400

    Added way to keep track of containers at locations

--- Backend/databaseDAOs/location.py
+++ Backend/databaseDAOs/location.py
@@ -5,25 +5,16 @@ class Location:
             self.listToLocation(args)
     
     def locationToList(self):
-        return (self.location_qrcode,self.description,self.lastPickup)
+        return (self.location_qrcode,self.description,self.lastPickup,self.containers)
 
     def listToLocation(self,list):
         self.location_qrcode = list[0]
         self.description = list[1]
         self.lastPickup = list[2]
+        self.containers = list[3]
 
     def dictToLocation(self,dict):
-        self.listToLocation((dict["location_qrcode"],dict["description"],dict["lastPickup"]))
+        self.listToLocation((dict["location_qrcode"],dict["description"],dict["lastPickup"],dict["containers"]))
 
     def locationToDict(self):
-        return {"location_qrcode": self.location_qrcode,"description": self.description,"lastPickip": self.lastPickup}
-        
-    def getQRcode(self):
-        return self.location_qrcode
-
-    def getDescription(self):
-        return self.description
-
-    def getLastPickup(self):
-        return self.lastPickup
-
+        return {"location_qrcode": self.location_qrcode,"description": self.description,"lastPickip": self.lastPickup,"containers": self.containers}
--- Backend/databaseDAOs/locationDAO.py
+++ Backend/databaseDAOs/locationDAO.py
@@ -35,7 +35,7 @@ class LocationDao(dao):
                 return myresult
             result = []
             for row in myresult[1]:
-                location = Location(row[0],row[1],row[2])
+                location = Location(row[0],row[1],row[2],int(row[3]))
                 result.append(location)
             return True, result
         except Exception as e:
@@ -51,7 +51,7 @@ class LocationDao(dao):
             if(myresult[0]==False):
                 return myresult 
             myresult2 = myresult[1][0] #myresult looks like (true,[(qrcode,des,lastpickup)])
-            location = Location(myresult2[0],myresult2[1],myresult2[2])
+            location = Location(myresult2[0],myresult2[1],myresult2[2],int(myresult2[3]))
             return True, location
         except Exception as e:
             logging.error("Error in selectByLocationQRcode")
@@ -65,7 +65,7 @@ class LocationDao(dao):
                 return myresult
             logging.info("Entering insertLocation")
             result = location.locationToList()
-            sql = "INSERT INTO location (location_qrcode, description, lastPickup) VALUES (%s,%s,%s)"
+            sql = "INSERT INTO location (location_qrcode, description, lastPickup, containers) VALUES (%s,%s,%s,%s)"
             myresult = self.handleSQL(sql,False,result)
             if(myresult[0] == False):
                 return myresult
@@ -76,16 +76,17 @@ class LocationDao(dao):
             logging.error(str(e))
             return self.handleError(e)
 
+
     def deleteLocation(self,location):
         try:
+            logging.info("Entering deleteLocation")
             myresult = self.checkFormatting(location)
             if(myresult[0] == False):
                 return myresult
             myresult = self.selectByLocationQRcode(location.location_qrcode)
             if(myresult[0]==False):
                 return myresult
-            logging.info("Entering deleteLocation")
-            sql = "DELETE FROM location WHERE location_qrcode = '" + location.getQRcode() + "'"
+            sql = "DELETE FROM location WHERE location_qrcode = '" + location.location_qrcode + "'"
             myresult = self.handleSQL(sql,False,None)
             if(myresult[0] == False):
                 return myresult
@@ -93,5 +94,22 @@ class LocationDao(dao):
             return True, ""
         except Exception as e:
             logging.error("Error in deleteLocation")
+            logging.error(str(e))
+            return self.handleError(e)
+
+    def updateContainers(self,location):
+        try:
+            logging.info("Entering updateContainers")
+            myresult = self.checkFormatting(location)
+            if(myresult[0] == False):
+                return myresult
+            sql = "UPDATE location SET containers = '" + str(location.containers) + "' where location_qrcode = '" + location.location_qrcode + "'"
+            myresult = self.handleSQL(sql,False,None)
+            if(myresult[0] == False):
+                return myresult
+            logging.info("updateContainers successful")
+            return True, ""
+        except Exception as e:
+            logging.error("Error in updateContainers")
             logging.error(str(e))
             return self.handleError(e)
