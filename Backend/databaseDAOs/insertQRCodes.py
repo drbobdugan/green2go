07b8e55e5196d5081b7babe1ce5086d37d4ade77
Elizabeth Blake <78761380+elizabeth-blake@users.noreply.github.com>
2021-04-15 14:06:51 -0400

    edit to strip qrcode string of \n

--- Backend/databaseDAOs/insertQRCodes.py
+++ Backend/databaseDAOs/insertQRCodes.py
@@ -8,16 +8,16 @@ from locationDAO import LocationDao
 
 import mysql.connector
 dao = ContainerDAO()
-dao2 = LocationDAO()
+#dao2 = LocationDAO()
 
 class insertQRcodes:
     def main():
         with open('containers.txt') as f:
             #print(f.read())
             for qrcode in f:
-                c = Container(qrcode)
+                c = Container(qrcode.rstrip("\n"))
                 dao.insertContainer(c)
-                print("successfully inserted", qrcode)
+                print("successfully inserted", qrcode.rstrip("\n"))
 
         """
         with open('locations.txt') as f2:
@@ -25,6 +25,6 @@ class insertQRcodes:
             for qrcode in f2:
                 l = Location(location_qrcode,description,lastPickup,containers)
                 dao2.insertLocation(l)
-                print("successfully inserted", location_qrcode)
+                print("successfully inserted", location_qrcode.rstrip("\n"))
         """
     main()
