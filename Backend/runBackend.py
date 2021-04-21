3f660f0ab0fbb65499f794567f5999e0981fce58
Andrew Petti <apetti1@students.stonehill.edu>
2021-04-21 16:23:08 -0400

    updating checkout tool

--- Backend/runBackend.py
+++ Backend/runBackend.py
@@ -178,7 +178,6 @@ def secretAddUser():
 def secretCheckout():
     return containerHandler.addRelationship(request, containerDao, relationshipDao)
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
 
