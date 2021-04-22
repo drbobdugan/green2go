21305955a575f6475f6affe581658e76cd21591f
ethan batt <>
2021-04-22 11:23:08 -0400

    tried to bring up code climate grade

--- Backend/handlers/locationHandler.py
+++ Backend/handlers/locationHandler.py
@@ -13,8 +13,7 @@ class LocationHandler:
         self.helperHandler = helperHandler
         self.locationdao = LocationDao()
 
-
-    def selectLocation(self, request,locationDao):
+    def locationcheckandAuth(self,request,locationDao):
         locationDic = None
         keys = ["qrcode",'email','auth_token']
         try:
@@ -23,22 +22,33 @@ class LocationHandler:
             self.helperHandler.falseQueryCheck(res)
         except Exception as e:
             return json.dumps({"success" : False, "message" : str(e)})
+        return(res)
+
+
+    def selectLocation(self, request,locationDao):
+        """ locationDic = None
+        keys = ["qrcode",'email','auth_token']
+        try:
+            locationDic = self.helperHandler.handleRequestAndAuth(request, keys)
+            res = self.locationdao.selectByLocationQRcode(locationDic['qrcode'])
+            self.helperHandler.falseQueryCheck(res)
+        except Exception as e:
+            return json.dumps({"success" : False, "message" : str(e)}) """
+        res=self.locationcheckandAuth(request,locationDao)
         res=res[0],res[1].locationToDict()
         return self.helperHandler.handleResponse(res)
         
     def clearlocation(self,request,locationDao):
-        locationDic = None
+        """ locationDic = None
         keys = ["qrcode",'email','auth_token']
         try:
             locationDic = self.helperHandler.handleRequestAndAuth(request, keys)
             res = self.locationdao.selectByLocationQRcode(locationDic['qrcode'])
             self.helperHandler.falseQueryCheck(res)
         except Exception as e:
-            return json.dumps({"success" : False, "message" : str(e)})
-        print(res[1].locationToDict())
+            return json.dumps({"success" : False, "message" : str(e)}) """
+        res =self.locationcheckandAuth(request,locationDao)
         res[1].lastPickup=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(res[1].locationToDict())
         res2=self.locationdao.updateLocation(res[1])
-        print("please",res2)
 
         return self.helperHandler.handleResponse(res2)
