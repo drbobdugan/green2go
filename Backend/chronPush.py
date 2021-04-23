13e3d4146672a943ac5f8718aa928ad66adb7207
root <oshea_patrick@hotmail.com>
2021-04-23 09:56:58 -0400

    fixing chronPush

--- Backend/chronPush.py
+++ Backend/chronPush.py
@@ -9,6 +9,9 @@ from relationshipDAO import RelationshipDAO
 need_to_be_messaged = []
 relationshipDao = RelationshipDAO()
 all_containers = relationshipDao.selectAll()[1]
+biggestSeen = 1
+batch = [[]]
+keys = {}
 for x in all_containers:
     if x['status']=='Checked Out':
       timeobj=datetime.datetime.strptime(x["statusUpdateTime"], '%Y-%m-%d %H:%M:%S')
@@ -16,6 +19,13 @@ for x in all_containers:
       future_date_and_time = timeobj + hours_added
       # it has been over 48 hours
       if (datetime.datetime.now() > future_date_and_time):
+        if x['email'] in keys:
+            keys[x['email']] += 1
+        else:
+            keys[x['email']] = 1
+        if keys[x['email']] > biggestSeen:
+            biggestSeen += 1
+            batch.append([])
         need_to_be_messaged.append(x['email'])
 for i in range(len(need_to_be_messaged)):
   need_to_be_messaged[i] = need_to_be_messaged[i].replace('.', '')
@@ -25,30 +35,34 @@ beams_client = PushNotifications(
     secret_key='8AC9B8AABB93DFE452B2EFC2714FCF923841B6740F97207F4512F240264FF493',
 )
 
-response = beams_client.publish_to_interests(
-  interests=need_to_be_messaged,
-  publish_body={
-    'apns': {
-      'aps': {
-        'alert': {
-          'title': 'choose2reuse',
-          'body': 'You have a container that has been checked out for over 48 hours... Please consider returning it...', 
-        },
-      },
-    },
-    'fcm': {
-      'notification': {
-        'title': 'choose2reuse',
-        'body': 'You have a container that has been checked out for over 48 hours... Please consider returning it...',
-      },
-    },
-    'web': {
-      'notification': {
-        'title': 'Hello',
-        'body': 'Hello, world!',
-      },
-    },
-  },
-)
+for key in keys:
+    for i in range(0, keys[key]):
+        batch[i].append(key)
 
-print(response['publishId'])
+for b in batch:
+    response = beams_client.publish_to_interests(
+        interests=b,
+        publish_body={
+            'apns': {
+                'aps': {
+                    'alert': {
+                        'title': 'choose2reuse',
+                        'body': 'You have a container that has been checked out for over 48 hours... Please consider returning it...', 
+                    },
+                },
+            },
+            'fcm': {
+                'notification': {
+                    'title': 'choose2reuse',
+                    'body': 'You have a container that has been checked out for over 48 hours... Please consider returning it...',
+                },
+            },
+            'web': {
+                'notification': {
+                    'title': 'Hello',
+                    'body': 'Hello, world!',
+                },
+            },
+        },
+    )
+    print(response['publishId'])
