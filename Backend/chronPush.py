b492d28fd6f5d7f45aeea18480e5d548013411d0
root <oshea_patrick@hotmail.com>
2021-05-06 20:01:22 -0400

    fixed chronPush

--- Backend/chronPush.py
+++ Backend/chronPush.py
@@ -13,39 +13,24 @@ containerList = []
 for item in all_containers:
     containerDic = item.relationshipToDict()
     containerList.append(containerDic)
-biggestSeen = 1
-batch = [[]]
-keys = {}
 for x in containerList:
     if x['status']=='Checked Out':
       timeobj=datetime.datetime.strptime(x["statusUpdateTime"], '%Y-%m-%d %H:%M:%S')
-      hours_added = datetime.timedelta(hours = 48)
+      hours_added = datetime.timedelta(minutes = 1)
       future_date_and_time = timeobj + hours_added
       # it has been over 48 hours
       if (datetime.datetime.now() > future_date_and_time):
-        if x['email'] in keys:
-            keys[x['email']] += 1
-        else:
-            keys[x['email']] = 1
-        if keys[x['email']] > biggestSeen:
-            biggestSeen += 1
-            batch.append([])
         need_to_be_messaged.append(x['email'])
 for i in range(len(need_to_be_messaged)):
   need_to_be_messaged[i] = need_to_be_messaged[i].replace('.', '')
-
 beams_client = PushNotifications(
     instance_id='7032df3e-e5a8-494e-9fc5-3b9f05a68e3c',
     secret_key='8AC9B8AABB93DFE452B2EFC2714FCF923841B6740F97207F4512F240264FF493',
 )
 
-for key in keys:
-    for i in range(0, keys[key]):
-        batch[i].append(key)
-
-for b in batch:
+for b in need_to_be_messaged:
     response = beams_client.publish_to_interests(
-        interests=b,
+        interests=[b],
         publish_body={
             'apns': {
                 'aps': {
