667a749741ea5d5401a1e56e1ddb6adcdbe62d90
Aaron <61666780+adjones722@users.noreply.github.com>
2021-05-19 14:10:57 -0400

    updating DAO.py

--- Backend/databaseDAOs/DAO.py
+++ Backend/databaseDAOs/DAO.py
@@ -10,23 +10,12 @@ class dao:
     def __init__(self):
         logging.basicConfig(filename='DAO.log', level=logging.DEBUG)
         self.database = "sys"
-        self.configData = {}
+        
 
     def changeDatabase(self,database):
         self.database = database
 
-    
-    def initializeConfigInfo(self):
-        os.chdir("/root")
-        path = os.path.abspath('credentials.txt')
-        with open(path) as file:
-            for line in file:
-                (key,value) = line.split()
-                self.configData[key] = value
-        file.close()
-        #return configData
 
-    
 
     def reconnectSql(self):
         try:
@@ -35,11 +24,11 @@ class dao:
             #logging.error("Error closing connection: Already disconnected")
             test = 1
 
-        self.initializeConfigInfo()
+        #self.initializeConfigInfo()
         self.mydb = mysql.connector.connect(
-            host = self.configData['host'],
-            user = self.configData['user'],
-            password= self.configData['password'],
+            host="198.199.77.174",
+            user="root",
+            password="Capstone2021!",
             database=self.database,
             buffered=True)
             
