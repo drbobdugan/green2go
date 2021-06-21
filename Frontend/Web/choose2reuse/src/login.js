75211e833c95a08d1d216dd330b1908d68a8da04
calebhadley1 <caleb@hadleynet.org>
2021-06-21 19:21:57 -0400

    testing get and post requests without http 3

--- Frontend/Web/choose2reuse/src/login.js
+++ Frontend/Web/choose2reuse/src/login.js
@@ -23,11 +23,13 @@ function Login (props) {
             'password' : password
         }
         try{
-        var response = await axios.post('https://choose2reuse.org/login', obj)
+        //var response = await axios.post('http://198.199.77.174:5000/login', obj)
+        var response = await axios.post('https://choose2reuse.org:5000/login', obj)
         console.log(response)
         console.log(response.data)
-        var user_response = await axios.get('http://choose2reuse.org/getUser?email='+ email + '&auth_token=' + response.data.data.auth_token)
-            if(response.data.success && user_response.data.data.role === 'Admin'){
+        //var user_response = await axios.get('http://198.199.77.174:5000/getUser?email='+ email + '&auth_token=' + response.data.data.auth_token)
+        var user_response = await axios.get('https://choose2reuse.org:5000/getUser?email='+ email + '&auth_token=' + response.data.data.auth_token)
+        if(response.data.success && user_response.data.data.role === 'Admin'){
                 setAuthToken(response.data.data.auth_token)
                 routeChange(response.data.data.auth_token)
             }else{
